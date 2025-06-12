604ec8d0dfd73086fa0a38b8e210c2d58771f0c2
João Fidelis <jfidelis999@gmail.com>
2025-06-12 16:18:27 -0300

    Create project

---
+++ src/test/java/org/example/workshopspringboot3jpa/WorkshopSpringboot3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workshopspringboot3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot3JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
