ea7d1c9cbf089e92d9c20379edaee5ecd9e56005
João Fidelis <jfidelis999@gmail.com>
2025-06-12 22:55:16 -0300

    Service layer, component registration

--- src/main/java/org/example/workshopspringboot3jpa/repositories/UserRepository.java
+++ src/main/java/org/example/workshopspringboot3jpa/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package org.example.workshopspringboot3jpa.repositories;
 
 import org.example.workshopspringboot3jpa.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
